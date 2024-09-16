e8db69d88cb29488f91cd37d8691fc11882f09fe
metarapi <dino.metarapi@gmail.com>
2024-09-16 13:13:29 +0200

    Reapply .gitignore rules

--- landing-page/src/index.js
+++
@@ -1,5 +0,0 @@
-import './styles.css';
-//import * as THREE from 'three';
-import scene from './scene';
-
-scene();
--- landing-page/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{html,js}',  './dist/**/*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
