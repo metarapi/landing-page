cffd20cc26106979cb428a2f249ab87096899f45
metarapi <dino.metarapi@gmail.com>
2024-11-08 23:21:24 +0100

    Add dropdown menu functionality

--- landing-page/src/index.js
+++ landing-page/src/index.js
@@ -3,3 +3,19 @@ import './styles.css';
 import scene from './scene';
 
 scene();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dropDownButton = document.getElementById('drop-down-button');
+    const dropdownMenu = document.getElementById('portfolio-dropdown');
+    
+    dropDownButton.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('hidden');
+    });
+
+    // Optionally, close the dropdown when clicking outside
+    document.addEventListener('click', (event) => {
+        if (!dropDownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.add('hidden');
+        }
+    });
+});
